02bf4b31791b1cab000942c243a93b1bb08dc1ed
Cass Beckford <CassJBeckford282@outlook.com>
2024-11-11 14:05:58 +0000

    styled the game cards and allow them a flip effect

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+const menuSection = {
+    instructions: 'instructions',
+    modeSelection: 'mode-selection'
+};
+
+function showSection(sectionId){
+    let sections = document.getElementById('menu-section').children;
+    for (let section of sections) {
+        /** modified code from https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp */
+        if (section.id === "menu-title"){
+            section.classList.add('visible');
+        }
+        else if (section.id !== sectionId){
+            section.classList.add('hidden');
+        }
+        else {
+            section.classList.add('visible');
+        }
+    }
+}
+
+function makeRulesVisible() {
+    showSection(menuSection.instructions);
+}
+
+function selectDifficulty() {
+    showSection(menuSection.modeSelection);
+}
+
+function createBoard(){
+    let gameboard = document.getElementById("game-board");
+    gameboard.innerHTML = "";
+    
+    for (let i = 0; i < 20; i++) {
+        gameboard.innerHTML += 
+        `<div class="card">
+            <div class="card-inner">
+                <div class="card-front">
+                    <img src="img/card-back.png" alt="back" style="width:200px;height:300px;">
+                </div>
+                <div class="card-back">
+                    <img src="img/Peach.jpg" alt="peach" style="width:200px;height:300px;">
+                </div>
+            </div>
+        </div>` 
+    }
+}
+
